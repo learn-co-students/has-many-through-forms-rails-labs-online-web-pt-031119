f728ff4725dd78be5cf0356f265f6df339bbf75f
Ryan <kerneltux0@gmail.com>
2019-08-07 12:49:11 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @comment = Comment.new
-    @comments = Comment.all
-    # @user = User.find(@post.user_id)
+    # @comment.user.build
   end
 
   def index
