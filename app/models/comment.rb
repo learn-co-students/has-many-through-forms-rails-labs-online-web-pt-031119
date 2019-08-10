87d97135c760de8ce408671943fb3046a1dd6cb3
“Mark <mkanko66@gmail.com>
2019-08-09 21:47:51 -0700

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,8 +3,8 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   accepts_nested_attributes_for :user 
 
-  def users_attributes=(user_attributes)
-    binding.pry 
+  def user_attributes=(user_attributes)
+    #binding.pry 
       self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank? 
     
   end
