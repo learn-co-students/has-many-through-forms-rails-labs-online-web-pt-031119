6a67e95b375e282d82cdba01d2ba6f336198b575
“Mark <mkanko66@gmail.com>
2019-08-09 21:06:45 -0700

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,18 +4,19 @@ class CommentsController < ApplicationController
   
 
   def create
-    @comment = Comment.new(comment_params)
-
-    if @comment.save
-      redirect_to post_path(@comment.post)
-    else
-      redirect_to post_path(params[:post_id]) 
-    end 
+  
+    @comment = Comment.create(comment_params)
+#binding.pry 
+    # if @comment.save
+       redirect_to post_path(@comment.post)
+    # else
+    #   redirect_to post_path(params[:post_id]) 
+    # end 
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes: [:username])
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,12 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user 
+
+  def users_attributes=(user_attributes)
+    binding.pry 
+      self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank? 
+    
+  end
 
 end
